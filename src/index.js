e3b75c3b800acd5a7ddb73d57291345be6dbe9fe
Aristela Marku <aristela.marku@gmail.com>
2021-09-28 11:31:46 +0200

    task listener finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const formElem=document.querySelector("form" )
+  formElem.addEventListener('submit',function(e){
+    e.preventDefault()
+    todoFUn(e.target['new-task-description'].value)
+  })
+})
+
+function todoFUn(todo){   
+  const p=document.createElement('p')
+  p.textContent=todo
+  console.log(p)
+  document.querySelector("#create-task-form").appendChild(p)
+  
+  const btn=document.createElement('button')
+  btn.addEventListener('click',(e)=>{
+    e.target.parentNode.remove();
+  })
+  btn.textContent='x'
+  p.appendChild(btn) 
+
+}
+
+
+
